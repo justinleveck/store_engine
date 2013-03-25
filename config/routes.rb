151643c0ca4ad0bd6c4fe2477c9a8002e8b94417
vagrant <vagrant@rails-starter-box.(none)>
2013-03-25 01:31:30 +0000

    Renamed product controller to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 StoreEngine::Application.routes.draw do
+  get "products/index"
+
+  get "products/show"
+
+  get "products/new"
+
+  get "products/edit"
+
+  get "products/create"
+
+  get "products/update"
+
+  get "products/destroy"
+
   get "product/index"
 
   get "product/show"
