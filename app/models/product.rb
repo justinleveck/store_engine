087bd89ba67ca77b87325fbfe1d7043f59804be8
Justin Leveck <jleveck@gmail.com>
2013-03-25 23:45:08 +0000

    Added decimal numeric validation for price

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :title
   validates_uniqueness_of :title
+  validates :price, 
+            :format => { :with => /^\d+??(?:\.\d{2,2})?$/ }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -31,13 +31,24 @@ describe Product do
     expect(product).to have(1).errors_on(:title)
   end
   it "is valid when price is a valid decimal numeric value" do
-    pending
+    product = Product.new(
+      title: 'Test',
+      description: 'Test description',
+      price: '1.99')
+    expect(product).to be_valid
   end
+  it "is invalid when price is an invalid decimal numeric value" do
+    product = Product.new(
+      title: 'Test',
+      description: 'Test description',
+      price: '1.9')
+    expect(product).to have(1).errors_on(:price)
+  end  
   it "is invalid when price is less than 0" do
     product = Product.new(
       title: 'Test',
       description: 'Test description',
-      price: '-5')
+      price: '-1.99')
     expect(product).to have(1).errors_on(:price)
   end
 end
