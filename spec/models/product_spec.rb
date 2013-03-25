f6391840c1880d2234adee90eb97e3d8ba93f6e9
vagrant <vagrant@rails-starter-box.(none)>
2013-03-25 08:06:19 +0000

    Added tests. 2 more need to pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+include Sorcery::TestHelpers::Rails
 
 # A product must have a title, description, and price.
 # The title and description cannot be empty strings.
@@ -16,5 +17,27 @@ describe Product do
   end
   it "is invalid without a title" do
     expect(Product.new(title: nil)).to have(1).errors_on(:title)
+  end
+  it "is invalid without a description" do
+    expect(Product.new(description: nil)).to have(1).errors_on(:description)
+  end
+  it "is invalid with a duplicate title" do
+    Product.create(
+      title: 'Test', description: 'Test description', price: '1.99')
+    product = Product.new(
+      title: 'Test',
+      description: 'Test description',
+      price: '1.99')
+    expect(product).to have(1).errors_on(:title)
+  end
+  it "is valid when price is a valid decimal numeric value" do
+    pending
+  end
+  it "is invalid when price is less than 0" do
+    product = Product.new(
+      title: 'Test',
+      description: 'Test description',
+      price: '-5')
+    expect(product).to have(1).errors_on(:price)
   end
 end
