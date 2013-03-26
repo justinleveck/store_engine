d8bc04b859699c33b36a5e90fab2a889f81ab8ea
Justin Leveck <jleveck@gmail.com>
2013-03-26 00:07:40 +0000

    Added standard methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,15 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+    end    
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -10,6 +17,7 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def create
@@ -28,8 +36,26 @@ class ProductsController < ApplicationController
   end
 
   def update
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end    
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end    
   end
 end
